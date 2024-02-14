dce7a87567f3671329028130366ad80c49ac135a
Yutong Liu <lyutong034@gmail.com>
2024-02-14 00:53:29 -0500

    Pass two tests.

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,44 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+public class DelegationSortedIntList implements IntegerList{
 
+    private SortedIntList sortedIntList;
+
+    private int totalAdded;
+
+    public DelegationSortedIntList() {
+        sortedIntList = new SortedIntList();
+        totalAdded = 0;
+    }
+
+    public boolean add(int num) {
+        totalAdded++;
+        return sortedIntList.add(num);
+    }
+
+    public boolean addAll(IntegerList list) {
+        totalAdded = totalAdded + list.size();
+        return sortedIntList.addAll(list);
+    }
+
+    public int get(int index) {
+        return sortedIntList.get(index);
+    }
+
+    public boolean remove(int num) {
+        return sortedIntList.remove(num);
+    }
+
+    public boolean removeAll(IntegerList list) {
+        return sortedIntList.removeAll(list);
+    }
+
+    public int size() {
+        return sortedIntList.size();
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,27 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList extends SortedIntList {
+    private int totalAdded;
 
+    public InheritanceSortedIntList() {
+        super();
+        this.totalAdded = 0;
+    }
+
+    @Override
+    public boolean add(int num) {
+        this.totalAdded++;
+        return super.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        // this.totalAdded = this.totalAdded + list.size();
+        return super.addAll(list);
+    }
+
+    public int getTotalAdded() {
+        return this.totalAdded;
+    }
 }
